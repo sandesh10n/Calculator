b712bc7a3984e772cc633985fb774d94a7257635
sandesh10n <sandesh10n@gmail.com>
2026-02-11 19:03:46 +0545

    made operate function

--- main.js
+++ main.js
@@ -2,16 +2,32 @@ function add(num1, num2){
     return num1 + num2;
 }
 
-function subtract() {
+function subtract(num1, num2) {
     return num1 - num2;
 }
 
-function multiply() {
+function multiply(num1, num2) {
     return num1 * num2;
 }
 
-function divide() {
+function divide(num1, num2) {
     return num1 / num2;
 }
 
-console.log(add(2, 3));
+function operate(num1, num2, operator){
+    if(operator == "+"){
+        return (add(num1, num2));
+    }
+
+    else if(operator == "-"){
+        return (subtract(num1, num2));
+    }
+
+    else if(operator == "*"){
+        return (multiply(num1, num2));
+    }
+
+    else {
+        return (divide(num1, num2));
+    }
+}console.log(operate(2, 3, "*"));
