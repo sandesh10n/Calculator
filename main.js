3d03bb0044f18cf5ec4169e080a29a6e52bea821
sandesh10n <sandesh10n@gmail.com>
2026-02-11 22:24:25 +0545

    add js function

--- main.js
+++ main.js
@@ -14,20 +14,53 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(num1, num2, operator){
+function calculate(num1, operator, num2){
     if(operator == "+"){
-        return (add(num1, num2));
+        return add(num1, num2);
     }
 
     else if(operator == "-"){
-        return (subtract(num1, num2));
+        return subtract(num1, num2);
     }
 
     else if(operator == "*"){
-        return (multiply(num1, num2));
+        return multiply(num1, num2);
     }
 
     else {
-        return (divide(num1, num2));
+        return divide(num1, num2);
     }
-}console.log(operate(2, 3, "*"));
+}
+
+function operate() {
+    let expression = display.value;   // e.g. "12+5"
+    let operator;
+
+    // find operator
+    if (expression.includes("+")) operator = "+";
+    else if (expression.includes("-")) operator = "-";
+    else if (expression.includes("*")) operator = "*";
+    else if (expression.includes("/")) operator = "/";
+    else {
+        display.value = "Error";
+        return;
+    }
+
+    // split numbers
+    let parts = expression.split(operator);
+    let num1 = parseFloat(parts[0]);
+    let num2 = parseFloat(parts[1]);
+
+    let result = calculate(num1, operator, num2);
+    display.value = result;
+}
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+
+function clearDisplay(){
+    display.value = "";
+}
+
