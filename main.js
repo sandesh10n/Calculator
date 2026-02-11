48bcdbcc585505bcc69208476eb3a0b58a140af1
sandesh10n <sandesh10n@gmail.com>
2026-02-11 18:38:58 +0545

    made basic operation function

---
+++ main.js
@@ -0,0 +1,17 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract() {
+    return num1 - num2;
+}
+
+function multiply() {
+    return num1 * num2;
+}
+
+function divide() {
+    return num1 / num2;
+}
+
+console.log(add(2, 3));
